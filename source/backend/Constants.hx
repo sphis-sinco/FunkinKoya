813a1130527650411e41e7305737e2c46eba30ac
Macohi <sinconsistencia@gmail.com>
2026-01-28 17:53:49 -0600

    week 1 update label

--- source/backend/Constants.hx
+++ source/backend/Constants.hx
@@ -8,7 +8,14 @@ class Constants
 
 	static function get_VERSION():String
 	{
-		return Application.current.meta.get('version');
+		return Application.current.meta.get('version') + VERSION_SUFFIX;
+	}
+
+	public static var VERSION_SUFFIX(get, never):String;
+
+	static function get_VERSION_SUFFIX():String
+	{
+		return ' : Week 1 Update';
 	}
 
 	public static var WINDOW_TITLE(get, never):String;
