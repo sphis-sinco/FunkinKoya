e1043c50fc90f8fdaa6dd0bc06130c30fee713f7
Macohi <sinconsistencia@gmail.com>
2026-01-28 18:00:53 -0600

    temp grid bg

--- source/frontend/freeplay/FreeplayState.hx
+++ source/frontend/freeplay/FreeplayState.hx
@@ -1,11 +1,16 @@
 package frontend.freeplay;
 
+import flixel.FlxG;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import backend.AssetPaths;
+import flixel.addons.display.FlxGridOverlay;
 import backend.CoolUtil;
 
 class FreeplayState extends MusicBeatState
 {
 	public var songList:Array<String> = [];
+
+	public var songText:FlxTypedGroup<Alphabet>;
 
 	override function create()
 	{
@@ -13,6 +18,13 @@ class FreeplayState extends MusicBeatState
 
 		songList = CoolUtil.coolTextFile(AssetPaths.txt('data/freeplaySonglist'));
 		trace(songList);
+
+		var GRID_SIZE = 32;
+		var gridBG = FlxGridOverlay.create(GRID_SIZE, GRID_SIZE, GRID_SIZE * Std.int(FlxG.width / GRID_SIZE) + 2, GRID_SIZE * Std.int(FlxG.height / GRID_SIZE) + 2);
+		add(gridBG);
+
+		songText = new FlxTypedGroup<Alphabet>();
+		add(songText);
 	}
 
 	override function update(elapsed:Float)
