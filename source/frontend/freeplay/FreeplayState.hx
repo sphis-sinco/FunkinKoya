921acb4ae913057d2403cf824cd580f9f30cb549
Macohi <sinconsistencia@gmail.com>
2026-01-28 18:02:40 -0600

    usin alphabet

--- source/frontend/freeplay/FreeplayState.hx
+++ source/frontend/freeplay/FreeplayState.hx
@@ -20,11 +20,22 @@ class FreeplayState extends MusicBeatState
 		trace(songList);
 
 		var GRID_SIZE = 32;
-		var gridBG = FlxGridOverlay.create(GRID_SIZE, GRID_SIZE, GRID_SIZE * Std.int(FlxG.width / GRID_SIZE) + 2, GRID_SIZE * Std.int(FlxG.height / GRID_SIZE) + 2);
+		var gridBG = FlxGridOverlay.create(GRID_SIZE, GRID_SIZE, GRID_SIZE * Std.int(FlxG.width / GRID_SIZE) + 2,
+			GRID_SIZE * Std.int(FlxG.height / GRID_SIZE) + 2);
 		add(gridBG);
 
 		songText = new FlxTypedGroup<Alphabet>();
 		add(songText);
+
+		var i = 0;
+		for (song in songList)
+		{
+			var songAlphabet:Alphabet = new Alphabet(24, 24 + (i * 64), song.toLowerCase(), true);
+			songAlphabet.ID = i;
+			songText.add(songAlphabet);
+
+			i++;
+		}
 	}
 
 	override function update(elapsed:Float)
