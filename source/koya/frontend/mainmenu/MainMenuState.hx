4e53e50c062cdc4b2ab5d5a12db0f76d904dae4e
Macohi <sinconsistencia@gmail.com>
2026-01-31 12:29:43 -0600

    main menu menu bgs

--- source/koya/frontend/mainmenu/MainMenuState.hx
+++ source/koya/frontend/mainmenu/MainMenuState.hx
@@ -1,3 +1,24 @@
 package koya.frontend.mainmenu;
 
-class MainMenuState extends MusicBeatState {}
+class MainMenuState extends MusicBeatState {
+
+	public var pinkBG:MenuBG = new MenuBG(true);
+	public var flashBG:MenuBG = new MenuBG(false);
+
+	override function create() {
+		super.create();
+
+		flashBG.color = 0x645B9A;
+		add(flashBG);
+		add(pinkBG);
+
+		flashBG.screenCenter();
+		pinkBG.screenCenter();
+
+		flashBG.scale.set(.75, .75);
+		pinkBG.scale.set(.75, .75);
+
+		flashBG.scrollFactor.set(0, .1);
+		pinkBG.scrollFactor.set(0, .1);
+	}
+}
