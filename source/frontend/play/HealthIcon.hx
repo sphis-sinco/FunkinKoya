3d0a1462e332af15c27ab0c2d44489f3b6f51034
Macohi <sinconsistencia@gmail.com>
2026-01-30 21:52:03 -0600

    base implementation :D

--- source/frontend/play/HealthIcon.hx
+++ source/frontend/play/HealthIcon.hx
@@ -3,33 +3,41 @@ package frontend.play;
 import backend.AssetPaths;
 import flixel.FlxSprite;
 
-class HealthIcon extends FlxSprite
+class HealthIcon extends FunkinSprite
 {
+	public var state(default, set):HealthIconState;
+
+	function set_state(state:HealthIconState):HealthIconState
+	{
+		switch (state)
+		{
+			// case WINNING: playAnim('');
+			case LOSING: playAnim('losing');
+			case _: playAnim('normal');
+		}
+
+		return state;
+	}
+
 	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
 		super();
-		loadGraphic(AssetPaths.image('iconGrid'), true, 150, 150);
 
-		animation.add('bf', [0, 1], 0, false, isPlayer);
-		animation.add('bf-car', [0, 1], 0, false, isPlayer);
-		animation.add('bf-christmas', [0, 1], 0, false, isPlayer);
-		animation.add('bf-pixel', [21, 21], 0, false, isPlayer);
-		animation.add('spooky', [2, 3], 0, false, isPlayer);
-		animation.add('pico', [4, 5], 0, false, isPlayer);
-		animation.add('mom', [6, 7], 0, false, isPlayer);
-		animation.add('mom-car', [6, 7], 0, false, isPlayer);
-		animation.add('tankman', [8, 9], 0, false, isPlayer);
-		animation.add('face', [10, 11], 0, false, isPlayer);
-		animation.add('dad', [12, 13], 0, false, isPlayer);
-		animation.add('senpai', [22, 22], 0, false, isPlayer);
-		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
-		animation.add('spirit', [23, 23], 0, false, isPlayer);
-		animation.add('bf-old', [14, 15], 0, false, isPlayer);
-		animation.add('gf', [16], 0, false, isPlayer);
-		animation.add('parents-christmas', [17], 0, false, isPlayer);
-		animation.add('monster', [19, 20], 0, false, isPlayer);
-		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
-		animation.play(char);
+		frames = AssetPaths.fromSparrow('healthIcons/$char', 'characters');
+		
+		addPrefixAnim('normal', '$char normal');
+		addPrefixAnim('losing', '$char lose');
+
+		updateHitbox();
+
+		flipX = isPlayer;
 		scrollFactor.set();
 	}
+}
+
+enum HealthIconState
+{
+	WINNING;
+	NORMAL;
+	LOSING;
 }
