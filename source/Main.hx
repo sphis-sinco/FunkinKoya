6b5f0917359ed931c1f29315cc50c5165a54c18c
Macohi <sinconsistencia@gmail.com>
2026-01-30 22:15:07 -0600

    yoinked psych engine crash handler :]

--- source/Main.hx
+++ source/Main.hx
@@ -1,9 +1,23 @@
 package;
 
+import lime.app.Application;
 import backend.InitState;
 import flixel.FlxGame;
 import openfl.display.FPS;
 import openfl.display.Sprite;
+
+
+//crash handler stuff
+#if CRASH_HANDLER
+import openfl.events.UncaughtErrorEvent;
+import haxe.CallStack;
+import openfl.Lib;
+import haxe.io.Path;
+import sys.io.File;
+import sys.FileSystem;
+#end
+
+using StringTools;
 
 class Main extends Sprite
 {
@@ -15,5 +29,56 @@ class Main extends Sprite
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
 		#end
+		
+		#if CRASH_HANDLER
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+		#end
 	}
+
+	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
+	// very cool person for real they don't get enough credit for their work
+	#if CRASH_HANDLER
+	function onCrash(e:UncaughtErrorEvent):Void
+	{
+		var errMsg:String = "";
+		var path:String = './crash';
+		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		var dateNow:String = Date.now().toString();
+
+		dateNow = dateNow.replace(" ", "_");
+		dateNow = dateNow.replace(":", "'");
+
+		#if debug
+		path += '-debug';
+		#end
+
+		path += '/KoyaCrash_$dateNow.txt';
+
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(s, file, line, column):
+					errMsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+			}
+		}
+
+		errMsg += "\nUncaught Error: " + e.error;
+		errMsg += "\nPlease report this error to the GitHub page: https://github.com/sphis-sinco/FunkinKoya/issues";
+		errMsg += "\n\n> Crash Handler written by: sqirra-rng, used by Psych Engine, and modified for Koya";
+
+		if (!FileSystem.exists("./crash/"))
+			FileSystem.createDirectory("./crash/");
+
+		File.saveContent(path, errMsg + "\n");
+
+		Sys.println(errMsg);
+		Sys.println("Crash dump saved in " + Path.normalize(path));
+
+		Application.current.window.alert(errMsg, "Error!");
+		Sys.exit(1);
+	}
+	#end
 }
