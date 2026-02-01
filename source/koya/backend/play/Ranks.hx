b5bf7597bda72b75c783b204a11ebb3d9b50ac16
Macohi <sinconsistencia@gmail.com>
2026-02-01 11:34:43 -0600

    ranks

---
+++ source/koya/backend/play/Ranks.hx
@@ -0,0 +1,13 @@
+package koya.backend.play;
+
+enum abstract Ranks(String) from String to String
+{
+	var BAD = 'BAD';
+	var OK = 'OK';
+
+	var GOOD = 'GOOD';
+	var GREAT = 'GREAT';
+	
+	var EXCELLENT = 'EXCELLENT';
+	var AMAZING = 'AMAZING';
+}
