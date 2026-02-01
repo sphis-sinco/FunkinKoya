37e88227c758749a9161de55bcd1b25615b216f7
Macohi <sinconsistencia@gmail.com>
2026-02-01 08:54:41 -0600

    its gfVersion not player 2 lol

--- source/koya/backend/tasks/ResaveAllSongs.hx
+++ source/koya/backend/tasks/ResaveAllSongs.hx
@@ -36,13 +36,17 @@ class ResaveAllSongs
 				{
 					case 'south', 'spookeez':
 						myJSON.stage = 'halloween';
+						
 						myJSON.player1 = 'bf-spooky';
-						myJSON.player2 = 'gf-spooky';
+						myJSON.player2 = 'spooky';
+						myJSON.gfVersion = 'gf-spooky';
 					case 'monster':
 						myJSON.stage = 'halloween';
 						myJSON.authors = 'Bassetfilms';
+
 						myJSON.player1 = 'bf-spooky';
-						myJSON.player2 = 'gf-spooky-monster';
+						myJSON.player2 = 'monster';
+						myJSON.gfVersion = 'gf-spooky-monster';
 				}
 
 				myJSON.generatedBy = '${Constants.SONG_GENERATED_BY_PREFIX}Task: Resave All Songs';
