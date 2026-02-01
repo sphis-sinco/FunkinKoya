5c974cddf5d9307f5a4104eeb52a017087d049fa
Macohi <sinconsistencia@gmail.com>
2026-02-01 07:57:40 -0600

    re-exports

--- source/koya/backend/tasks/ResaveAllSongs.hx
+++ source/koya/backend/tasks/ResaveAllSongs.hx
@@ -13,7 +13,7 @@ class ResaveAllSongs
 	{
 		#if sys
 		var songList = SongList.songList;
-		var fixedSongs = [];
+		var reexported = [];
 		for (song in songList)
 		{
 			// trace(song);d
@@ -22,37 +22,10 @@ class ResaveAllSongs
 			for (difficulty in Difficulty.list)
 			{
 				// trace('song: ${song.song} | difficulty: $difficulty');
-				var myJSONnoFix = Song.loadFromJson(Highscore.formatSong(song.song.toLowerCase(), difficulty), song.song.toLowerCase(), false);
 				var myJSON = Song.loadFromJson(Highscore.formatSong(song.song.toLowerCase(), difficulty), song.song.toLowerCase());
 				myJSON.difficulty = difficulty;
 
 				if (myJSON == null) continue;
-				if (myJSONnoFix == null) continue; // there should be no difference between them
-
-				var fieldCheck:Int = Reflect.fields(myJSONnoFix).length;
-
-				for (field in Reflect.fields(myJSONnoFix))
-				{
-					if (field == 'version') continue;
-
-					var mjf:String = Std.string(Reflect.field(myJSON, field)).trim();
-					var mjnff:String = Std.string(Reflect.field(myJSONnoFix, field)).trim();
-
-					if (mjf != mjnff)
-					{
-						fieldCheck--;
-						if (!['notes'].contains(field))
-							trace('${song.song.toLowerCase()}${difficulty.chartSuffix()} : "$field : ${mjf}" != "$field : ${mjnff}"');
-						else
-							trace('${song.song.toLowerCase()}${difficulty.chartSuffix()} : "fixed : $field" != "notfixed : $field"');
-					}
-					else
-					{
-						// trace('${song.song.toLowerCase()}${difficulty.chartSuffix()} : "fixed : $field" == "notfixed : $field"');
-					}
-				}
-
-				if (fieldCheck < Reflect.fields(myJSONnoFix).length) fixedSongs.push(myJSON.song);
 
 				// songList.push(myJSON);
 				var curSong = myJSON.song.toLowerCase();
@@ -63,20 +36,25 @@ class ResaveAllSongs
 				{
 					case 'south', 'spookeez':
 						myJSON.stage = 'halloween';
+						myJSON.player1 = 'bf';
+						myJSON.player2 = 'gf-spooky';
 					case 'monster':
 						myJSON.stage = 'halloween';
 						myJSON.authors = 'Bassetfilms';
+						myJSON.player1 = 'bf';
+						myJSON.player2 = 'gf-spooky-monster';
 				}
 
 				myJSON.generatedBy = '${Constants.SONG_GENERATED_BY_PREFIX}Task: Resave All Songs';
 
 				// trace('saving $path');
 				sys.io.File.saveContent(path, Json.stringify({song: myJSON}, '\t'));
+				reexported.push(myJSON.song);
 			}
 		}
-		if (fixedSongs.length < 1) trace('Task Uncompleted : No Songs with (important) changes');
+		if (reexported.length < 1) trace('Task Boringly Completed : No Songs reexported');
 		else
-			trace('Task Completed : Reexported ${fixedSongs.length} song charts with (important) changes');
+			trace('Task Completed : Reexported ${reexported.length} song charts');
 		Sys.exit(0);
 		#else
 		trace('Task Uncompleted : Not Sys');
