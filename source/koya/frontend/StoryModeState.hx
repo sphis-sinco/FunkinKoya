ab7149ead1ebe17c7d4bfb8133082e50d0efafa6
Macohi <sinconsistencia@gmail.com>
2026-01-31 23:57:28 -0600

    difficulty sprite

--- source/koya/frontend/StoryModeState.hx
+++ source/koya/frontend/StoryModeState.hx
@@ -1,5 +1,6 @@
 package koya.frontend;
 
+import koya.backend.Constants;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import koya.backend.play.Difficulty;
@@ -17,6 +18,12 @@ class StoryModeState extends MenuState
 		this.itemIncOffset += 100;
 	}
 
+	var arrow_UP:ArrowUI = new ArrowUI(UP, Constants.UI_ARROW_SKIN_DIFFICULTY_SELECT);
+	var arrow_DOWN:ArrowUI = new ArrowUI(DOWN, Constants.UI_ARROW_SKIN_DIFFICULTY_SELECT);
+
+	var aU_y:Float = 0.0;
+	var aD_y:Float = 0.0;
+
 	override function create()
 	{
 		super.create();
@@ -31,6 +38,23 @@ class StoryModeState extends MenuState
 		add(bottomBorder);
 		bottomBorder.screenCenter(X);
 		bottomBorder.y = FlxG.height - bottomBorder.height;
+
+		for (arrow in [arrow_DOWN, arrow_UP])
+		{
+			arrow.screenCenter();
+			arrow.y = FlxG.height;
+			add(arrow);
+		}
+
+		arrow_UP.y -= arrow_UP.height * 4;
+		arrow_DOWN.y -= arrow_DOWN.height * 2;
+
+		aU_y = arrow_UP.y;
+		aD_y = arrow_DOWN.y;
+
+		songDifficultySprite = new DifficultySprite(currentDifficulty);
+		add(songDifficultySprite);
+		songDifficultySprite.screenCenter();
 	}
 
 	override function accept(item:String)
@@ -57,5 +81,20 @@ class StoryModeState extends MenuState
 
 			FlxG.switchState(() -> new PlayState());
 		}
+	}
+
+	public var songDifficultySprite:DifficultySprite;
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (currentDifficulty < 0) currentDifficulty = 0;
+		if (currentDifficulty > Difficulty.list.length - 1) currentDifficulty = Difficulty.list.length - 1;
+
+		currentDifficultyEnum = currentDifficulty;
+
+		songDifficultySprite.difficulty = currentDifficulty;
+		songDifficultySprite.y = FlxG.height - songDifficultySprite.height * 4;
 	}
 }
