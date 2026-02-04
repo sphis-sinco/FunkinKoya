6817558d5fd02539d1fe8f02e7e02d0e6b51cede
Macohi <sinconsistencia@gmail.com>
2026-02-03 19:21:04 -0600

    play anim (+ format change again hehehe)

--- source/koya/backend/songs/EventParser.hx
+++ source/koya/backend/songs/EventParser.hx
@@ -1,9 +1,30 @@
 package koya.backend.songs;
 
+import koya.frontend.scenes.play.PlayState;
+
 class EventParser
 {
 	public static function sendEvent(name:String, value:String)
 	{
 		name = name.toLowerCase();
+		var vals:Array<String> = value.split('/');
+
+		if (name == 'playanim') playAnim(vals);
+	}
+
+	public static function playAnim(values:Array<String>)
+	{
+		var character:String = values[0];
+		var animationName:String = values[1];
+
+		switch (character)
+		{
+			case '0', 'bf', 'boy', 'boyfriend', 'player':
+				PlayState.instance.currentStage.boyfriend?.playAnim(animationName);
+			case '1', 'dad', 'opponent':
+				PlayState.instance.currentStage.dad?.playAnim(animationName);
+			case '2', 'gf', 'girl', 'girlfriend', 'damsel':
+				PlayState.instance.currentStage.gf?.playAnim(animationName);
+		}
 	}
 }
