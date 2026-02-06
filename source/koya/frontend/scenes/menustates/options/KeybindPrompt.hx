e0775f2cc6df3bc6ba31dcc6f2445196a9e94b12
Macohi <sinconsistencia@gmail.com>
2026-02-06 17:06:17 -0600

    keybind changing works

--- source/koya/frontend/scenes/menustates/options/KeybindPrompt.hx
+++ source/koya/frontend/scenes/menustates/options/KeybindPrompt.hx
@@ -1,5 +1,10 @@
 package koya.frontend.scenes.menustates.options;
 
+import flixel.util.FlxTimer;
+import koya.backend.AssetPaths;
+import koya.backend.save.SaveField;
+import koya.backend.save.Save;
+import flixel.input.keyboard.FlxKey;
 import koya.frontend.ui.AtlasText;
 import flixel.tweens.FlxEase;
 import flixel.util.FlxColor;
@@ -11,6 +16,8 @@ using StringTools;
 class KeybindPrompt extends MusicBeatSubstate
 {
 	var promptText:AtlasText;
+	var bg:FunkinSprite = new FunkinSprite();
+	var colorShit:FunkinSprite = new FunkinSprite();
 
 	var keybind:String;
 
@@ -25,20 +32,16 @@ class KeybindPrompt extends MusicBeatSubstate
 	{
 		super.create();
 
-		var bg:FunkinSprite = new FunkinSprite();
 		bg.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		bg.alpha = 0;
 		bg.scrollFactor.set();
 		add(bg);
-		FlxTween.tween(bg, {alpha: 0.6}, 0.4, {ease: FlxEase.quartInOut});
 
-		var colorShit:FunkinSprite = new FunkinSprite();
 		colorShit.makeGraphic(FlxG.width, FlxG.height, FlxColor.fromString('#FF99CC'));
-		colorShit.scale.set(0.9,0.9);
+		colorShit.scale.set(0.9, 0.9);
 		colorShit.alpha = 0;
 		colorShit.scrollFactor.set();
 		add(colorShit);
-		FlxTween.tween(colorShit, {alpha: 0.8}, 0.6, {ease: FlxEase.quartInOut});
 
 		promptText = new AtlasText(10, 10, 'Binding: ' + '“$keybind”' + '\n\nESCAPE TO CANCEL', BOLD);
 		promptText.screenCenter();
@@ -47,6 +50,56 @@ class KeybindPrompt extends MusicBeatSubstate
 		promptText.alpha = 0;
 		promptText.color = FlxColor.WHITE;
 
+		FlxTween.tween(bg, {alpha: 0.6}, 0.4, {ease: FlxEase.quartInOut});
+		FlxTween.tween(colorShit, {alpha: 1}, 0.6, {ease: FlxEase.quartInOut});
 		FlxTween.tween(promptText, {alpha: 1}, 0.8, {ease: FlxEase.quartInOut});
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (controls.BACK) close();
+		else
+		{
+			if (!FlxG.keys.justReleased.ANY) return;
+
+			var invalids:Array<FlxKey> = [Z, ENTER, BACKSPACE, ESCAPE, P, ENTER];
+
+			for (keybind in Save.keybinds)
+				invalids.push(FlxKey.fromString(keybind.get()));
+
+			var key:FlxKey = cast FlxG.keys.firstJustReleased();
+
+			if (invalids.contains(key))
+			{
+				promptText.text = 'Not bound\n\nInvalid Key.';
+				close();
+				return;
+			}
+
+			var keyString = key.toString();
+
+			var keybindField:SaveField<String> = Reflect.getProperty(Save, keybind);
+			if (keybindField == null)
+			{
+				promptText.text = 'Not bound\n\nInvalid Keybind';
+				close();
+				return;
+			}
+
+			keybindField.set(keyString);
+			promptText.text = 'Bound to “$keyString';
+
+			FlxG.sound.play(AssetPaths.sound('confirmMenu', 'ui'));
+
+			FlxTween.tween(bg, {alpha: 0}, 0.8, {ease: FlxEase.quartInOut});
+			FlxTween.tween(colorShit, {alpha: 0}, 0.6, {ease: FlxEase.quartInOut});
+			FlxTween.tween(promptText, {alpha: 0}, 0.4, {ease: FlxEase.quartInOut});
+
+			FlxTimer.wait(1, () -> {
+				close();
+			});
+		}
 	}
 }
