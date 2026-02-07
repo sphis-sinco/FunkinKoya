8b7dcea6cd7221e0b37b9e43cba60c6abcaf2bb6
Macohi <sinconsistencia@gmail.com>
2026-02-07 11:36:32 -0600

    play title jingle

--- source/koya/frontend/scenes/SplashScene.hx
+++ source/koya/frontend/scenes/SplashScene.hx
@@ -1,5 +1,7 @@
 package koya.frontend.scenes;
 
+import koya.backend.AssetPaths;
+import flixel.FlxG;
 import flixel.util.typeLimit.NextState;
 
 class SplashScene extends MusicBeatState
@@ -11,5 +13,12 @@ class SplashScene extends MusicBeatState
 		super();
 
 		this.nextScene = nextScene;
+	}
+
+	override function create()
+	{
+		super.create();
+
+		FlxG.sound.play(AssetPaths.music('TitleJingle'));
 	}
 }
