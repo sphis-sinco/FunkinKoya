3051c51925d0d9327107f7bf4dd171248fd15036
Macohi <sinconsistencia@gmail.com>
2026-02-07 11:49:22 -0600

    splash texts

--- source/koya/frontend/scenes/SplashScene.hx
+++ source/koya/frontend/scenes/SplashScene.hx
@@ -1,14 +1,22 @@
 package koya.frontend.scenes;
 
+import koya.frontend.ui.AtlasText;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import koya.backend.CoolUtil;
 import koya.backend.Conductor;
 import flixel.util.FlxTimer;
 import koya.backend.AssetPaths;
 import flixel.FlxG;
 import flixel.util.typeLimit.NextState;
 
+using StringTools;
+
 class SplashScene extends MusicBeatState
 {
 	public var nextScene:NextState;
+
+	public var splashText:Array<String> = CoolUtil.coolTextFile(AssetPaths.txt('data/splash'));
+	public var splashTexts:FlxTypedGroup<AtlasText> = new FlxTypedGroup<AtlasText>();
 
 	override public function new(nextScene:NextState)
 	{
@@ -27,5 +35,31 @@ class SplashScene extends MusicBeatState
 			});
 		});
 		Conductor.changeBPM(120.0);
+
+		add(splashTexts);
+	}
+
+	override function stepHit()
+	{
+		super.stepHit();
+
+		var splash:String = splashText[curStep] ?? '';
+
+		if (splash == '') return;
+		
+		if (splash.startsWith('-'))
+		{
+			if (splashTexts.members[splashTexts.length - 1] != null) splashTexts.members[splashTexts.length - 1].text += splash.substring(1);
+		}
+		else
+		{
+			var newText:AtlasText = new AtlasText(0, 0, splash, BOLD);
+			newText.screenCenter();
+
+			for (atlasText in splashTexts.members)
+				atlasText.y -= newText.height;
+
+			splashTexts.add(newText);
+		}
 	}
 }
